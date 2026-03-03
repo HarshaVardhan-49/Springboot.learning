0b8e203711965f01efa91fa62a829ecb2ab1c424
HarshaVardhan-49 <harsharelangi43@gmail.com>
2026-03-03 07:26:29 -0600

    day1 - first spring boot endpoint

---
+++ src/main/java/com/harshh/StudentAPI/HelloController.java
@@ -0,0 +1,12 @@
+package com.harshh.StudentAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Harsha ! Springboot Application is wroking....!";
+    }
+}

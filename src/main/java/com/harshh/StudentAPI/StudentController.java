3853ea469439ce0944da63145a0e1f330e8eb9f6
HarshaVardhan-49 <harsharelangi43@gmail.com>
2026-03-11 02:58:11 -0500

    Day 3 - REST endPoints GET PUSH DETELE

--- src/main/java/com/harshh/StudentAPI/StudentController.java
+++ src/main/java/com/harshh/StudentAPI/StudentController.java
@@ -1,21 +1,35 @@
 package com.harshh.StudentAPI;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/students")
 public class StudentController {
      StudentServices studentServices;
 
      public StudentController(StudentServices studentServices){
          this.studentServices = studentServices;
      }
-
-     @GetMapping("/students")
+     // GET all studends (read)
+     @GetMapping
     public List<String> getStudents(){
          return studentServices.getStudents();
      }
-
+     // GET Students by ID (students/id)
+     @GetMapping("/{id}")
+    public String getStudentId(@PathVariable int id){
+         return studentServices.getStudentId(id);
+     }
+    // Post Student nae (create student)
+     @PostMapping
+    public String addStudent(@RequestBody String name){
+         return studentServices.addStudent(name);
+     }
+    // Delete student by id
+     @DeleteMapping("/{id}")
+    public String deleteStudentId(@PathVariable int id){
+         return studentServices.deleteStudentId(id);
+     }
 }
--- src/main/java/com/harshh/StudentAPI/StudentServices.java
+++ src/main/java/com/harshh/StudentAPI/StudentServices.java
@@ -6,11 +6,28 @@ import java.util.ArrayList;
 
 @Service
 public class StudentServices {
+    private List<String> students = new ArrayList<>(
+        List.of("Harsha","Junnu","vamsi")
+    );
+
     public List<String> getStudents(){
-        List<String> students = new ArrayList<>();
-        students.add("Harsha");
-        students.add("junnu");
-        students.add("vamsi");
         return students;
+    }
+
+    public String getStudentId(int id) {
+        if(id < 0 || id >= students.size()) return "Student not found";
+        return students.get(id);
+    }
+
+    public String addStudent(String name) {
+        students.add(name);
+        return name + "succesfull added";
+    }
+
+    public String deleteStudentId(int id) {
+        if(id < 0 || id >= students.size()) return "Student not found";
+        String removed = students.get(id);
+        students.remove(id);
+        return removed + "removed Succesfully";
     }
 }
